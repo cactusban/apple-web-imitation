70377df8153b17c35ad838e233c8ab47647d00b5
cactusban <98244159+cactusban@users.noreply.github.com>
2025-09-26 09:59:28 +0800

    完善mac页面

--- src/components/maccomponents/maccontainer/productdetail/index.tsx
+++ src/components/maccomponents/maccontainer/productdetail/index.tsx
@@ -1,11 +1,49 @@
-import { memo, type ReactNode } from 'react'
-import { Container, ProductContainer } from './style'
+import { memo, useEffect, useRef, useState, type ReactNode } from 'react'
+import { Container, FloatingMenu, ProductContainer } from './style'
 
 interface Iprops {
   children?: ReactNode
 }
 
 const ProductDetail: React.FC<Iprops> = () => {
+  const [activeTab, setActiveTab] = useState<'macbook' | 'imac' | 'display'>(
+    'macbook'
+  )
+  const handleTabClick = (tab: 'macbook' | 'imac' | 'display') => {
+    setActiveTab(tab)
+  }
+  const getSliderTransform = () => {
+    switch (activeTab) {
+      case 'macbook':
+        return 'translateX(0)'
+      case 'imac':
+        return 'translateX(138px)'
+      case 'display':
+        return 'translateX(273px)'
+    }
+  }
+  const hrRef = useRef<HTMLHRElement>(null)
+  const [showMenu, setShowMenu] = useState(false)
+  const floatMenuRef = useRef<HTMLDivElement>(null)
+  const downRef = useRef<HTMLHeadingElement>(null)
+  useEffect(() => {
+    const handleScroll = () => {
+      if (hrRef.current) {
+        const hrRect = hrRef.current.getBoundingClientRect()
+        setShowMenu(hrRect.top <= 130)
+      }
+      if (downRef.current) {
+        const downRect = downRef.current.getBoundingClientRect()
+        if (downRect.top <= 50) {
+          setShowMenu(false)
+        }
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
   return (
     <>
       <Container>
@@ -13,11 +51,420 @@ const ProductDetail: React.FC<Iprops> = () => {
           <h1>全系产品细细看</h1>
           <h3>比较各款机型 {'>'}</h3>
         </div>
+        <FloatingMenu className={showMenu ? 'show' : 'hide'}>
+          {activeTab === 'macbook' ? (
+            <>
+              <div className="container">
+                <div className="content">
+                  <h1>MacBook Air</h1>
+                  <h1>13 英寸和 15 英寸</h1>
+                  <h3>RMB 333/月或 RMB 7999 起*</h3>
+                </div>
+                <div className="content">
+                  {' '}
+                  <h1>MacBook Pro</h1>
+                  <h1>14 英寸和 16 英寸</h1>
+                  <h3>RMB 542/月或 RMB 12999 起*</h3>
+                </div>
+              </div>
+            </>
+          ) : activeTab === 'imac' ? (
+            <>
+              <div className="container">
+                <div className="content">
+                  <h1>MacBook Air</h1>
+                  <h1>13 英寸和 15 英寸</h1>
+                  <h3>RMB 333/月或 RMB 7999 起*</h3>
+                </div>
+                <div className="content">
+                  {' '}
+                  <h1>MacBook Pro</h1>
+                  <h1>14 英寸和 16 英寸</h1>
+                  <h3>RMB 542/月或 RMB 12999 起*</h3>
+                </div>
+              </div>
+            </>
+          ) : activeTab === 'display' ? (
+            <>
+              <div className="container">
+                <div className="content">
+                  <h1>MacBook Air</h1>
+                  <h1>13 英寸和 15 英寸</h1>
+                  <h3>RMB 333/月或 RMB 7999 起*</h3>
+                </div>
+                <div className="content">
+                  {' '}
+                  <h1>MacBook Pro</h1>
+                  <h1>14 英寸和 16 英寸</h1>
+                  <h3>RMB 542/月或 RMB 12999 起*</h3>
+                </div>
+              </div>
+            </>
+          ) : null}
+        </FloatingMenu>
         <ProductContainer>
           <div className="tab-navigation">
-            <div className="tab-button">笔记本电脑</div>
-            <div className="tab-button">台式电脑</div>
-            <div className="tab-button">显示器</div>
+            <div
+              className="slider"
+              style={{ transform: getSliderTransform() }}
+            ></div>
+            <div
+              className={
+                activeTab === 'macbook' ? 'tab-button active' : 'tab-button'
+              }
+              onClick={() => handleTabClick('macbook')}
+            >
+              笔记本电脑
+            </div>
+            <div
+              className={
+                activeTab === 'imac' ? 'tab-button active' : 'tab-button'
+              }
+              onClick={() => handleTabClick('imac')}
+            >
+              台式电脑
+            </div>
+            <div
+              className={
+                activeTab === 'display' ? 'tab-button active' : 'tab-button'
+              }
+              onClick={() => handleTabClick('display')}
+            >
+              显示器
+            </div>
+          </div>
+          <div className="product-list">
+            {activeTab === 'macbook' && (
+              <>
+                <ul className="product">
+                  <li className="product-item">
+                    {' '}
+                    <img
+                      src="https://www.apple.com.cn/assets-www/en_WW/mac/product_tile/large/mba_13_15_297e3c914_2x.png"
+                      alt=""
+                    />
+                    <div className="content">
+                      {' '}
+                      <h1>MacBook Air</h1>
+                      <h1>13 英寸和 15 英寸</h1>
+                      <h2>M4 芯片</h2>
+                      <h3>超薄超快超便携， </h3>
+                      <h3>玩转工作创作和娱乐。</h3>
+                      <h2>RMB 333/月或 RMB 7999 起</h2>
+                      <div className="btn">
+                        {' '}
+                        <button>进一步了解</button>
+                        <h4>购买</h4>
+                      </div>
+                    </div>
+                    <hr ref={hrRef} />
+                    <div className="after">
+                      <h1>13.6 英寸或 15.3 英寸</h1>
+                      <h5>
+                        Liquid 视网膜显示屏亮度达 500 尼特，支持 10 亿色彩
+                      </h5>
+                      <img
+                        src="https://www.apple.com.cn/assets-www/en_WW/mac/tout_table/chip.m4.fill_elevated_0b86b1a9b.svg"
+                        alt=""
+                      />
+                      <h5>Apple M4 芯片</h5>
+                      <h5>最长达</h5>
+                      <h1>18 小时</h1>
+                      <h5>电池续航</h5>
+                      <div className="double">
+                        {' '}
+                        <h1>四个端口</h1>
+                        <h5>两个雷雳 4 (USB‑C) 端口、耳机插孔、MagSafe 端口</h5>
+                      </div>
+                      <h1>1.24 千克或 1.51 千克</h1>
+                      <h5 ref={downRef}>重量</h5>
+                    </div>
+                  </li>
+                  <li className="product-item">
+                    {' '}
+                    <img
+                      src="https://www.apple.com.cn/assets-www/en_WW/mac/product_tile/large/mbp_14_16_84af52ffc_2x.png"
+                      alt=""
+                    />
+                    <div className="content">
+                      <h1>MacBook Pro </h1>
+                      <h1>14 英寸和 16 英寸</h1>
+                      <h2>M4、M4 Pro 或 M4 Max 芯片</h2>
+                      <h3>超先进的 Mac 笔电，</h3>
+                      <h3>艰巨任务巨拿手。</h3>
+                      <h2>RMB 542/月或 RMB 12,999 起</h2>
+                      <div className="btn">
+                        {' '}
+                        <button>进一步了解</button>
+                        <h4>购买</h4>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="after">
+                      <h1>13.6 英寸或 15.3 英寸</h1>
+                      <h5>
+                        Liquid 视网膜显示屏亮度达 500 尼特，支持 10 亿色彩
+                      </h5>
+                      <img
+                        src="https://www.apple.com.cn/assets-www/en_WW/mac/tout_table/chip.m4.fill_elevated_0b86b1a9b.svg"
+                        alt=""
+                      />
+                      <h5>Apple M4 芯片</h5>
+                      <h5>最长达</h5>
+                      <h1>18 小时</h1>
+                      <h5>电池续航</h5>
+
+                      <div className="double">
+                        {' '}
+                        <h1>四个端口</h1>
+                        <h5>
+                          三个雷雳 4 (USB‑C) 端口或三个雷雳 5 (USB‑C) 端口、
+                        </h5>
+                      </div>
+                      <h1>1.55 千克或 2.14 千克</h1>
+                      <h5 ref={downRef}>重量</h5>
+                    </div>
+                  </li>
+                </ul>
+              </>
+            )}
+            {activeTab === 'imac' && (
+              <>
+                <ul className="product">
+                  <li className="product-item">
+                    {' '}
+                    <img
+                      src="https://www.apple.com.cn/assets-www/en_WW/mac/product_tile/large/imac_24_6233f1ec7_2x.png"
+                      alt=""
+                    />
+                    <div className="content">
+                      <h1>MacBook Pro </h1>
+                      <h1>14 英寸和 16 英寸</h1>
+                      <h2>M4、M4 Pro 或 M4 Max 芯片</h2>
+                      <h3>超先进的 Mac 笔电，</h3>
+                      <h3>艰巨任务巨拿手。</h3>
+                      <h2>RMB 542/月或 RMB 12,999 起</h2>
+                      <div className="btn">
+                        {' '}
+                        <button>进一步了解</button>
+                        <h4>购买</h4>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="after">
+                      <h1>13.6 英寸或 15.3 英寸</h1>
+                      <h5>
+                        Liquid 视网膜显示屏亮度达 500 尼特，支持 10 亿色彩
+                      </h5>
+                      <img
+                        src="https://www.apple.com.cn/assets-www/en_WW/mac/tout_table/chip.m4.fill_elevated_0b86b1a9b.svg"
+                        alt=""
+                      />
+                      <h5>Apple M4 芯片</h5>
+                      <h5>最长达</h5>
+                      <h1>18 小时</h1>
+                      <h5>电池续航</h5>
+
+                      <div className="double">
+                        {' '}
+                        <h1>四个端口</h1>
+                        <h5>
+                          三个雷雳 4 (USB‑C) 端口或三个雷雳 5 (USB‑C) 端口、
+                        </h5>
+                      </div>
+                      <h1>1.55 千克或 2.14 千克</h1>
+                      <h5 ref={downRef}>重量</h5>
+                    </div>
+                  </li>
+                  <li className="product-item">
+                    {' '}
+                    <img
+                      src="https://www.apple.com.cn/assets-www/en_WW/mac/product_tile/large/mac_mini_eda8b2c0c_2x.png"
+                      alt=""
+                    />
+                    <div className="content">
+                      <h1>MacBook Pro </h1>
+                      <h1>14 英寸和 16 英寸</h1>
+                      <h2>M4、M4 Pro 或 M4 Max 芯片</h2>
+                      <h3>超先进的 Mac 笔电，</h3>
+                      <h3>艰巨任务巨拿手。</h3>
+                      <h2>RMB 542/月或 RMB 12,999 起</h2>
+                      <div className="btn">
+                        {' '}
+                        <button>进一步了解</button>
+                        <h4>购买</h4>
+                      </div>
+                    </div>
+                    <hr ref={hrRef} />
+                    <div className="after">
+                      <h1>13.6 英寸或 15.3 英寸</h1>
+                      <h5>
+                        Liquid 视网膜显示屏亮度达 500 尼特，支持 10 亿色彩
+                      </h5>
+                      <img
+                        src="https://www.apple.com.cn/assets-www/en_WW/mac/tout_table/chip.m4.fill_elevated_0b86b1a9b.svg"
+                        alt=""
+                      />
+                      <h5>Apple M4 芯片</h5>
+                      <h5>最长达</h5>
+                      <h1>18 小时</h1>
+                      <h5>电池续航</h5>
+
+                      <div className="double">
+                        {' '}
+                        <h1>四个端口</h1>
+                        <h5>
+                          三个雷雳 4 (USB‑C) 端口或三个雷雳 5 (USB‑C) 端口、
+                        </h5>
+                      </div>
+                      <h1>1.55 千克或 2.14 千克</h1>
+                      <h5 ref={downRef}>重量</h5>
+                    </div>
+                  </li>
+                  <li className="product-item">
+                    {' '}
+                    <img
+                      src="https://www.apple.com.cn/assets-www/en_WW/mac/product_tile/large/mac_studio_af116abb2_2x.png"
+                      alt=""
+                    />
+                    <div className="content">
+                      <h1>MacBook Pro </h1>
+                      <h1>14 英寸和 16 英寸</h1>
+                      <h2>M4、M4 Pro 或 M4 Max 芯片</h2>
+                      <h3>超先进的 Mac 笔电，</h3>
+                      <h3>艰巨任务巨拿手。</h3>
+                      <h2>RMB 542/月或 RMB 12,999 起</h2>
+                      <div className="btn">
+                        {' '}
+                        <button>进一步了解</button>
+                        <h4>购买</h4>
+                      </div>
+                    </div>
+                    <hr />
+                    <div className="after">
+                      <h1>13.6 英寸或 15.3 英寸</h1>
+                      <h5>
+                        Liquid 视网膜显示屏亮度达 500 尼特，支持 10 亿色彩
+                      </h5>
+                      <img
+                        src="https://www.apple.com.cn/assets-www/en_WW/mac/tout_table/chip.m4.fill_elevated_0b86b1a9b.svg"
+                        alt=""
+                      />
+                      <h5>Apple M4 芯片</h5>
+                      <h5>最长达</h5>
+                      <h1>18 小时</h1>
+                      <h5>电池续航</h5>
+
+                      <div className="double">
+                        {' '}
+                        <h1>四个端口</h1>
+                        <h5>
+                          三个雷雳 4 (USB‑C) 端口或三个雷雳 5 (USB‑C) 端口、
+                        </h5>
+                      </div>
+                      <h1>1.55 千克或 2.14 千克</h1>
+                      <h5 ref={downRef}>重量</h5>
+                    </div>
+                  </li>
+                </ul>
+              </>
+            )}
+            {activeTab === 'display' && (
+              <>
+                <ul className="product">
+                  <li className="product-item">
+                    {' '}
+                    <img
+                      src="https://www.apple.com.cn/assets-www/en_WW/mac/product_tile/large/studio_display_6169ec976_2x.png"
+                      alt=""
+                    />
+                    <div className="content">
+                      <h1>MacBook Pro </h1>
+                      <h1>14 英寸和 16 英寸</h1>
+                      <h2>M4、M4 Pro 或 M4 Max 芯片</h2>
+                      <h3>超先进的 Mac 笔电，</h3>
+                      <h3>艰巨任务巨拿手。</h3>
+                      <h2>RMB 542/月或 RMB 12,999 起</h2>
+                      <div className="btn">
+                        {' '}
+                        <button>进一步了解</button>
+                        <h4>购买</h4>
+                      </div>
+                    </div>
+                    <hr ref={hrRef} />
+                    <div className="after">
+                      <h1>13.6 英寸或 15.3 英寸</h1>
+                      <h5>
+                        Liquid 视网膜显示屏亮度达 500 尼特，支持 10 亿色彩
+                      </h5>
+                      <img
+                        src="https://www.apple.com.cn/assets-www/en_WW/mac/tout_table/chip.m4.fill_elevated_0b86b1a9b.svg"
+                        alt=""
+                      />
+                      <h5>Apple M4 芯片</h5>
+                      <h5>最长达</h5>
+                      <h1>18 小时</h1>
+                      <h5>电池续航</h5>
+
+                      <div className="double">
+                        {' '}
+                        <h1>四个端口</h1>
+                        <h5>
+                          三个雷雳 4 (USB‑C) 端口或三个雷雳 5 (USB‑C) 端口、
+                        </h5>
+                      </div>
+                      <h1>1.55 千克或 2.14 千克</h1>
+                      <h5 ref={downRef}>重量</h5>
+                    </div>
+                  </li>
+                  <li className="product-item">
+                    {' '}
+                    <img
+                      src="https://www.apple.com.cn/assets-www/en_WW/mac/product_tile/large/pro_display_80f05c380_2x.png"
+                      alt=""
+                    />
+                    <div className="content">
+                      <h1>MacBook Pro </h1>
+                      <h1>14 英寸和 16 英寸</h1>
+                      <h2>M4、M4 Pro 或 M4 Max 芯片</h2>
+                      <h3>超先进的 Mac 笔电，</h3>
+                      <h3>艰巨任务巨拿手。</h3>
+                      <h2>RMB 542/月或 RMB 12,999 起</h2>
+                      <div className="btn">
+                        {' '}
+                        <button>进一步了解</button>
+                        <h4>购买</h4>
+                      </div>
+                    </div>
+                    <hr ref={hrRef} />
+                    <div className="after">
+                      <h1>13.6 英寸或 15.3 英寸</h1>
+                      <h5>
+                        Liquid 视网膜显示屏亮度达 500 尼特，支持 10 亿色彩
+                      </h5>
+                      <img
+                        src="https://www.apple.com.cn/assets-www/en_WW/mac/tout_table/chip.m4.fill_elevated_0b86b1a9b.svg"
+                        alt=""
+                      />
+                      <h5>Apple M4 芯片</h5>
+                      <h5>最长达</h5>
+                      <h1>18 小时</h1>
+                      <h5>电池续航</h5>
+
+                      <div className="double">
+                        {' '}
+                        <h1>四个端口</h1>
+                        <h5>
+                          三个雷雳 4 (USB‑C) 端口或三个雷雳 5 (USB‑C) 端口、
+                        </h5>
+                      </div>
+                      <h1>1.55 千克或 2.14 千克</h1>
+                      <h5 ref={downRef}>重量</h5>
+                    </div>
+                  </li>
+                </ul>
+              </>
+            )}
           </div>
         </ProductContainer>
       </Container>
--- src/components/maccomponents/maccontainer/productdetail/style.ts
+++ src/components/maccomponents/maccontainer/productdetail/style.ts
@@ -26,15 +26,216 @@ export const Container = styled.div`
   }
 `
 export const ProductContainer = styled.div`
+  transition: all 0.3s ease;
   .tab-navigation {
+    position: relative;
     display: flex;
-    justify-content: center;
+    width: 400px;
+    margin-top: 50px;
     margin-bottom: 40px;
-    background: rgba(0, 0, 0, 0.05);
-    border-radius: 12px;
+    margin-left: 80px;
+    border-radius: 50px;
     padding: 4px;
+    background: rgba(0, 0, 0, 0.05);
+
+    .slider {
+      position: absolute;
+      top: 6px;
+      left: 4px;
+      width: 120px;
+      height: 45px;
+      background: rgb(26, 26, 26);
+      border-radius: 50px;
+      transition: transform 0.3s ease;
+      z-index: 1;
+    }
+
     .tab-button {
+      position: relative;
+      z-index: 2;
+      margin: 0 auto;
+      width: 120px;
+      height: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      border-radius: 50px;
+      font-size: 15px;
+      font-weight: 350;
+      transition: color 0.3s ease;
+      cursor: pointer;
+
+      &.active {
+        color: white;
+      }
+
+      &:not(.active) {
+        color: rgb(26, 26, 26);
+      }
+
+      &:hover:not(.active) {
+        color: rgb(60, 60, 60);
+      }
+    }
+  }
+  .product-list {
+    display: flex;
+    margin-left: 80px;
+    width: 75%;
+    justify-content: space-around;
+    flex-direction: column;
+    .menu {
+      position: fixed;
+      top: -60px; /* 初始隐藏在顶部 */
+      left: 0;
+      width: 100%;
+      background: white;
+      padding: 15px 0;
+      box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+
+      z-index: 1000;
+      text-align: center;
+      font-size: 18px;
+      font-weight: bold;
+      transition: all 0.3s ease;
+      &.hide {
+        top: -60px;
+        transition: top 1s ease-in-out;
+      }
+      &.show {
+        top: 0;
+      }
+    }
+    .product {
+      display: flex;
+      justify-content: flex-start;
+      align-items: end;
+
+      .product-item {
+        padding-right: 70px;
+        justify-content: end;
+        .after {
+          display: flex;
+          flex-direction: column;
+          justify-content: end;
+          align-items: center;
+          min-height: 100px;
+
+          .double {
+            display: flex;
+            flex-direction: column;
+            justify-content: end;
+            align-items: center;
+          }
+          h1 {
+            font-size: 22px;
+            font-weight: 580;
+            margin-top: 30px;
+            height: 50px;
+          }
+          img {
+            margin-top: 30px;
+            justify-content: center;
+            width: 50px;
+            height: 100%;
+          }
+          h5 {
+            font-size: 11px;
+            font-weight: 350;
+            margin-top: 10px;
+          }
+        }
+        img {
+          width: 300px;
+          align-items: end;
+        }
+        .content {
+          margin-top: 40px;
+          display: flex;
+          line-height: 30px;
+          flex-direction: column;
+          justify-content: end;
+          align-items: center;
+
+          h1 {
+            font-size: 22px;
+            font-weight: 580;
+          }
+          h2 {
+            font-size: 18px;
+            font-weight: 500;
+            margin: 10px;
+          }
+          h3 {
+            font-size: 15px;
+            font-weight: 350;
+          }
+          .btn {
+            margin-top: 20px;
+            display: flex;
+            margin-bottom: 20px;
+            button {
+              width: 130px;
+              height: 40px;
+              background-color: #0070c9;
+              border-radius: 50px;
+              color: white;
+              margin-right: 30px;
+              &:hover {
+                cursor: pointer;
+              }
+            }
+            h4 {
+              color: #0070c9;
+
+              font-weight: 500;
+              line-height: 40px;
+              &:hover {
+                text-decoration: underline;
+                cursor: pointer;
+              }
+            }
+          }
+        }
+      }
     }
   }
 `
-export const ProductDetail = styled.div``
+export const FloatingMenu = styled.div`
+  position: fixed;
+  left: 0;
+  height: 130px;
+  width: 100%;
+  background: #f5f5f5;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  transition: all 0.3s ease;
+  z-index: 1000;
+  text-align: center;
+  font-size: 18px;
+  font-weight: bold;
+  opacity: 0;
+  &.show {
+    top: 0;
+    transition: all 0.3s ease;
+    opacity: 1;
+  }
+  .container {
+    display: flex;
+    width: 65%;
+    margin-left: 40px;
+    margin-top: 20px;
+    justify-content: space-around;
+    .content {
+      align-items: center;
+      justify-content: center;
+      h1 {
+        font-size: 20px;
+        font-weight: 500;
+      }
+      h3 {
+        font-size: 14px;
+        font-weight: 350;
+      }
+    }
+  }
+`
